d7361c068d04352a28131919337d8e4b77afa55a
konradtrejo <kbtch95@gmail.com>
2018-11-05 04:06:58 -0500

    version 1.0

---
+++ shopy/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/konrad/Documentos/Benjamin/Computer   Science/programacion_backend/python/python-platzi/puls6/shopy/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
